a34043bd0991881b1319b19da6e4a980da1568ae
Nguyễn Thành Vinh <103758812+vinhvt619@users.noreply.github.com>
2024-02-19 00:46:05 +0700

    test code

---
+++ ass2/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream> //Khai báo thư viện(Các bạn nhớ cài đặt thư viện)
+#include<SDL2/SDL.h>
+
+
+// for rendering images and graphics on screen
+// #include <SDL2/SDL_image.h>
+ 
+// for using SDL_Delay() functions
+// #include <SDL2/SDL_timer.h>
+
+
+using namespace std;
+
+
+const int WIDTH = 800, HEIGHT = 600;
+
+//lưu ý: các biến trong hàm main là bắt buộc để chạy thư viện SDL
+int main( int argc, char* argv[] )
+{
+//    cout<<"hello"<<endl;
+//    return 0;
+//  Khởi tạo thư viện(Khởi động)
+//     if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+//     {
+//         return(1);
+//     }  
+
+//  // Đợi 5 giây trước khi đóng màn hình SDL
+//     SDL_Delay(5000);
+
+//  // Thoát khỏi SDL
+//     SDL_Quit();
+
+//     return 0;
+
+
+    // SDL_Init( SDL_INIT_EVERYTHING);
+
+    // SDL_Window *window = SDL_CreateWindow("HEllo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    // if(NULL == window){
+    //     cout << "there is no window" << SDL_GetError()<< endl;
+    // }
+
+    // SDL_Event windowEvent;
+
+    // while (true){
+    //     if(SDL_PollEvent(&windowEvent)){
+    //         if(SDL_QUIT == windowEvent.type){
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // SDL_DestroyWindow(window);
+    // SDL_Quit();
+    // return EXIT_SUCCESS;
+
+}
